5f90c4e32bf278227b595cd82cbbee391e6a69cf
Chenhao Liu <K21077228@kcl.ac.uk>
2022-10-19 23:08:11 +0100

    update Thing parameters 3

--- things/models.py
+++ things/models.py
@@ -1,16 +1,10 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-
-
-def inRange(integer):
-    if 101 > integer >= 0:
-        return True
-    return False
 
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.CharField(max_length=120, blank=True, unique=False)
-    quantity = models.IntegerField(validators=[inRange])
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
