3f52af480d0f7475e6939bf9a562c15ff6151dd5
Chenhao Liu <K21077228@kcl.ac.uk>
2022-10-19 19:26:31 +0100

    update Ting class 2

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Thing(models.Model()):
+class Thing(models.Model):
     name = models.CharField()
     description = models.CharField()
     quantity = models.IntegerField()
