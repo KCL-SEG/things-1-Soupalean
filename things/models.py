27151d4e50711df9206bbb59ea6d19d9eded9aed
Chenhao Liu <K21077228@kcl.ac.uk>
2022-10-19 19:42:00 +0100

    update Thing parameters

--- things/models.py
+++ things/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 
 
+def inRange(integer):
+    if 101 > integer >= 0:
+        return integer
+    raise AttributeError("quantity out of range")
+
+
 class Thing(models.Model):
-    name = models.CharField(max_length=10)
-    description = models.CharField(max_length=50)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(validators=[inRange])
